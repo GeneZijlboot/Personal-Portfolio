33bbed4ebeeaaa0218caba0626788f2b3041a54b
GeneZijlboot <gene.cyberzijlmans@gmail.com>
2023-08-16 23:37:05 +0200

    form data send to my email

--- src/ChangeBGColor.js
+++ src/ChangeBGColor.js
@@ -2,7 +2,7 @@ export default function ChangeBg(){
     let NavBar = document.querySelector('.NavBar');
     const NavBarLinks = document.querySelector('.NavBarLinks');
     const Title = document.querySelector('.Title');
-    let Devide2 = document.querySelector('.Devide2');
+    let Tools = document.querySelector('.Tools');
     let AnotherTxt = document.querySelector('.AnotherTxt');
     let twoSkills = document.querySelector('.twoSkills');
 
@@ -19,14 +19,9 @@ export default function ChangeBg(){
         <a class="HyperLinkDark" href="#Projects"><svg class="icons" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path d="M392.8 1.2c-17-4.9-34.7 5-39.6 22l-128 448c-4.9 17 5 34.7 22 39.6s34.7-5 39.6-22l128-448c4.9-17-5-34.7-22-39.6zm80.6 120.1c-12.5 12.5-12.5 32.8 0 45.3L562.7 256l-89.4 89.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l112-112c12.5-12.5 12.5-32.8 0-45.3l-112-112c-12.5-12.5-32.8-12.5-45.3 0zm-306.7 0c-12.5-12.5-32.8-12.5-45.3 0l-112 112c-12.5 12.5-12.5 32.8 0 45.3l112 112c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256l89.4-89.4c12.5-12.5 12.5-32.8 0-45.3z"/></svg>Projects</a>
         <a class="HyperLinkDark" href="#Contact"><svg class="icons" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z"/></svg>Contact</a>
         `
-
-        AnotherTxt.style="color:white";
-        twoSkills.style="color:white";
-
-        Devide2.classList.remove('Devide2Dark')
-        Devide2.classList.add('Devide2')
-
-
+        AnotherTxt.style="color:white;";
+        twoSkills.style="color:white;";
+        Tools.style="color:rgb(197, 95, 18);";
     } else {
         document.body.style = "background-color: white; -webkit-transition: background-color 200ms linear;"; 
         NavBar.style = "background-color: white; -webkit-transition: background-color 200ms linear;";
@@ -38,11 +33,8 @@ export default function ChangeBg(){
             <a class="HyperLink" href="#Projects"><svg class="icons" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path d="M392.8 1.2c-17-4.9-34.7 5-39.6 22l-128 448c-4.9 17 5 34.7 22 39.6s34.7-5 39.6-22l128-448c4.9-17-5-34.7-22-39.6zm80.6 120.1c-12.5 12.5-12.5 32.8 0 45.3L562.7 256l-89.4 89.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l112-112c12.5-12.5 12.5-32.8 0-45.3l-112-112c-12.5-12.5-32.8-12.5-45.3 0zm-306.7 0c-12.5-12.5-32.8-12.5-45.3 0l-112 112c-12.5 12.5-12.5 32.8 0 45.3l112 112c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256l89.4-89.4c12.5-12.5 12.5-32.8 0-45.3z"/></svg>Projects</a>
             <a class="HyperLink" href="#Contact"><svg class="icons" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z"/></svg>Contact</a>
         `
-
-        AnotherTxt.style="color:rgb(30, 30, 30)";
-        twoSkills.style="color:rgb(30, 30, 30)";
-
-        Devide2.classList.add('Devide2Dark')
-        Devide2.classList.remove('Devide2')
+        AnotherTxt.style="color:rgb(30, 30, 30);";
+        twoSkills.style="color:rgb(30, 30, 30);";
+        Tools.style="color:rgb(13, 129, 59);";
     }
 }
