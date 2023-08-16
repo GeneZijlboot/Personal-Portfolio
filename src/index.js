40d3e46ada5f6ae29d01504c46075ed5f8e8c012
GeneZijlboot <gene.cyberzijlmans@gmail.com>
2023-08-16 15:42:36 +0200

    adding a dark background when scrolling + make the nav and text swap colors

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import './style.css';
 
 //import all the js files
 import TyperWriter from "../src/TypeWriter"
+import ChangeBg from "../src/ChangeBGColor"
 
 TyperWriter();
+window.addEventListener("scroll", ChangeBg);
 
-//npx webpack --watch
