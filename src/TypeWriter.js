f7790a678d88f43b52b3b91fa9de37f91d207793
GeneZijlboot <gene.cyberzijlmans@gmail.com>
2023-08-30 13:06:48 +0200

    final changes

--- src/TypeWriter.js
+++ src/TypeWriter.js
@@ -1,6 +1,6 @@
 export default function TyperWriter(){
     var typed = new Typed('#element', {
-        strings: ['Front-End developer', 'Back-End developer'],
+        strings: ['Front-End developer'],
         typeSpeed: 120,
         backSpeed: 70,
         loop: true
